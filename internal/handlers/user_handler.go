685a80682d7bab5fb1186dd2cebe799a5ed704d0
yasinhessnawi1 <79910859+yasinhessnawi1@users.noreply.github.com>
2024-04-25 13:03:23 +0200

    (#6) implemented the users endpoint for the interactions with database.Added methods : POST,GET,DELETE

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"E-Commerce_Website_Database/internal/models"
+	"E-Commerce_Website_Database/internal/tools"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
@@ -14,4 +16,56 @@ func GetUsers(c *gin.Context, db *gorm.DB) {
 		return
 	}
 	c.JSON(http.StatusOK, users)
+}
+
+// in /internal/handlers/user_handler.go
+func CreateUser(c *gin.Context, db *gorm.DB) {
+	var newUser models.UserDB
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var user models.User
+	user.User_ID = tools.GenerateUUID()
+	user.Username = newUser.Username
+	user.Password = newUser.Password
+	user.Email = newUser.Email
+	user.First_Name = newUser.First_Name
+	user.Last_Name = newUser.Last_Name
+	user.Address = newUser.Address
+	fmt.Println(user)
+	if err := db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+}
+func UpdateUser(c *gin.Context, db *gorm.DB) {
+	var user models.User
+	id := c.Param("id")
+	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	var newUser models.UserDB
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user.Username = newUser.Username
+	user.Password = newUser.Password
+	user.Email = newUser.Email
+	user.First_Name = newUser.First_Name
+	user.Last_Name = newUser.Last_Name
+	user.Address = newUser.Address
+	db.Save(&user)
+	c.JSON(http.StatusOK, user)
+}
+func DeleteUser(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	if err := db.Where("id = ?", id).Delete(&models.User{}).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error deleting user"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted"})
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -2,22 +2,27 @@ package models
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type User struct {
-	BaseModel
-	Username  string `gorm:"unique"`
-	Password  string
-	Email     string `gorm:"unique"`
-	FirstName string
-	LastName  string
-	Address   string
+	gorm.Model
+	User_ID    uint32 `json:"user_id"`
+	Username   string `gorm:"unique" json:"username"`
+	Password   string `json:"password"`
+	Email      string `gorm:"unique" json:"email"`
+	First_Name string `json:"first_name"`
+	Last_Name  string `json:"last_name"`
+	Address    string `json:"address"`
 }
-type BaseModel struct {
-	ID        uint `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+
+type UserDB struct {
+	gorm.Model
+	Username   string `gorm:"unique" json:"username"`
+	Password   string `json:"password"`
+	Email      string `gorm:"unique" json:"email"`
+	First_Name string `json:"first_name"`
+	Last_Name  string `json:"last_name"`
+	Address    string `json:"address"`
 }
 
 func GetAllUsers(db *gorm.DB) ([]User, error) {
