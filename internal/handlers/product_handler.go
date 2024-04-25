7820f134cbcc2c9522155ffc05a2c952b82517a4
yasinhessnawi1 <79910859+yasinhessnawi1@users.noreply.github.com>
2024-04-25 13:04:22 +0200

    (#9) implemented the brands endpoint for the interactions with database.Added methods : POST,GET,DELETE

--- internal/handlers/product_handler.go
+++ internal/handlers/product_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"E-Commerce_Website_Database/internal/models"
+	"E-Commerce_Website_Database/internal/tools"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
@@ -14,4 +15,54 @@ func GetProducts(c *gin.Context, db *gorm.DB) {
 		return
 	}
 	c.JSON(http.StatusOK, products)
+}
+func CreateProduct(c *gin.Context, db *gorm.DB) {
+	var newProduct models.ProductDB
+	if err := c.ShouldBindJSON(&newProduct); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var product models.Product
+	product.Product_ID = tools.GenerateUUID()
+	product.Name = newProduct.Name
+	product.Description = newProduct.Description
+	product.Price = newProduct.Price
+	product.Stock_quantity = newProduct.Stock_quantity
+	product.Brand_ID = newProduct.Brand_ID
+	product.Category_ID = newProduct.Category_ID
+
+	if err := db.Create(&product).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, newProduct)
+}
+func UpdateProduct(c *gin.Context, db *gorm.DB) {
+	var product models.Product
+	id := c.Param("id")
+	if err := db.Where("id = ?", id).First(&product).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		return
+	}
+	var newProduct models.ProductDB
+	if err := c.ShouldBindJSON(&newProduct); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	product.Name = newProduct.Name
+	product.Description = newProduct.Description
+	product.Price = newProduct.Price
+	product.Stock_quantity = newProduct.Stock_quantity
+	product.Brand_ID = newProduct.Brand_ID
+	product.Category_ID = newProduct.Category_ID
+	db.Save(&product)
+	c.JSON(http.StatusOK, product)
+}
+func DeleteProduct(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	if err := db.Where("id = ?", id).Delete(&models.Product{}).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error deleting product"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Product deleted"})
 }
--- internal/models/product.go
+++ internal/models/product.go
@@ -4,14 +4,25 @@ import (
 	"gorm.io/gorm"
 )
 
+type ProductDB struct {
+	gorm.Model
+	Name           string  ` json:"name"`
+	Description    string  `json:"description"`
+	Price          float64 `json:"price"`
+	Stock_quantity int     `json:"stock_quantity"`
+	Brand_ID       uint32  `json:"brand_id"`
+	Category_ID    uint32  `json:"category_id"`
+}
+
 type Product struct {
-	BaseModel
-	Name          string
-	Description   string
-	Price         float64
-	StockQuantity int
-	BrandID       uint
-	CategoryID    uint
+	gorm.Model
+	Product_ID     uint32  `json:"product_id"`
+	Name           string  ` json:"name"`
+	Description    string  `json:"description"`
+	Price          float64 `json:"price"`
+	Stock_quantity int     `json:"stock_quantity"`
+	Brand_ID       uint32  `json:"brand_id"`
+	Category_ID    uint32  `json:"category_id"`
 }
 
 func GetAllProducts(db *gorm.DB) ([]Product, error) {
