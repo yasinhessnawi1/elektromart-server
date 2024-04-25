423f3ae8ce430960ec36dcfcfef88cb7cdda56cc
yasinhessnawi1 <79910859+yasinhessnawi1@users.noreply.github.com>
2024-04-25 13:06:14 +0200

    (#10) implemented the payments endpoint for the interactions with database.Added methods : POST,GET,DELETE

--- internal/handlers/payment_handler.go
+++ internal/handlers/payment_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"E-Commerce_Website_Database/internal/models"
+	"E-Commerce_Website_Database/internal/tools"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
@@ -14,4 +15,62 @@ func GetPayments(c *gin.Context, db *gorm.DB) {
 		return
 	}
 	c.JSON(http.StatusOK, payments)
+}
+
+func CreatePayment(c *gin.Context, db *gorm.DB) {
+	var newPayment models.PaymentDB
+	if err := c.ShouldBindJSON(&newPayment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var payment models.Payment
+	payment.Payment_ID = tools.GenerateUUID()
+	payment.Order_ID = newPayment.Order_ID
+	payment.Payment_method = newPayment.Payment_method
+	payment.Amount = newPayment.Amount
+	payment.Payment_date = newPayment.Payment_date
+	payment.Status = newPayment.Status
+	if err := db.Create(&payment).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, payment)
+}
+
+func UpdatePayment(c *gin.Context, db *gorm.DB) {
+	var updatedPayment models.PaymentDB
+	if err := c.ShouldBindJSON(&updatedPayment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var payment models.Payment
+	id := c.Param("id")
+	if err := db.Where("id = ?", id).First(&payment).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Payment not found"})
+		return
+	}
+	payment.Payment_ID = tools.GenerateUUID()
+	payment.Order_ID = updatedPayment.Order_ID
+	payment.Payment_method = updatedPayment.Payment_method
+	payment.Amount = updatedPayment.Amount
+	payment.Payment_date = updatedPayment.Payment_date
+	payment.Status = updatedPayment.Status
+	if err := db.Where("id = ?", id).Updates(&payment).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, payment)
+}
+
+func DeletePayment(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	if err := db.Where("id = ?", id).First(&models.Payment{}).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Payment not found"})
+		return
+	}
+	if err := db.Where("id = ?", id).Delete(&models.Payment{}).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
 }
--- internal/models/payment.go
+++ internal/models/payment.go
@@ -4,13 +4,23 @@ import (
 	"gorm.io/gorm"
 )
 
+type PaymentDB struct {
+	gorm.Model
+	Order_ID       uint32  `json:"order_id"`
+	Payment_method string  `json:"payment_method"`
+	Amount         float64 `json:"amount"`
+	Payment_date   string  `json:"payment_date"`
+	Status         string  `json:"status"`
+}
+
 type Payment struct {
-	BaseModel
-	OrderID       uint
-	PaymentMethod string
-	Amount        float64
-	PaymentDate   string
-	Status        string
+	gorm.Model
+	Payment_ID     uint32  `json:"payment_id"`
+	Order_ID       uint32  `json:"order_id"`
+	Payment_method string  `json:"payment_method"`
+	Amount         float64 `json:"amount"`
+	Payment_date   string  `json:"payment_date"`
+	Status         string  `json:"status"`
 }
 
 func GetAllPayments(db *gorm.DB) ([]Payment, error) {
