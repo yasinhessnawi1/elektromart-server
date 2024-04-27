060227b841914a1d9fcbc2d9613260126fdde84e
Rami Hail Amer <ramiha@stud.ntnu.no>
2024-04-27 03:53:53 +0200

    (#21) refactoring category_handler.go (CreateCategory) for checking all inputs data.

--- internal/handlers/category_handler.go
+++ internal/handlers/category_handler.go
@@ -9,6 +9,18 @@ import (
 	"net/http"
 )
 
+func GetCategory(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	var category models.Category
+
+	if err := db.Where("id = ?", id).First(&category).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Category not found"})
+		return
+	}
+	c.JSON(http.StatusOK, category)
+
+}
+
 func GetCategories(c *gin.Context, db *gorm.DB) {
 	categories, err := models.GetAllCategories(db)
 	if err != nil {
@@ -18,34 +30,34 @@ func GetCategories(c *gin.Context, db *gorm.DB) {
 	c.JSON(http.StatusOK, categories)
 }
 
-func GetCategory(c *gin.Context, db *gorm.DB) {
-	id := c.Param("id")
-	var category models.Category
-	if err := db.Where("id = ?", id).First(&category).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Category not found"})
-		return
-	}
-	c.JSON(http.StatusOK, category)
-
-}
 func CreateCategory(c *gin.Context, db *gorm.DB) {
 	var newCategory models.Category
 	if err := c.ShouldBindJSON(&newCategory); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	fmt.Println(newCategory)
-	var category models.Category
-	category.Model.ID = uint(tools.GenerateUUID())
-	category.Name = newCategory.Name
-	category.Description = newCategory.Description
 
-	if err := db.Create(&category).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	category := models.Category{
+		Name:        newCategory.Name,
+		Description: newCategory.Description,
+		Model: gorm.Model{
+			ID: uint(tools.GenerateUUID()),
+		},
+	}
+
+	if failed, err := checkCategory(category, newCategory, db); failed {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Validation error", "details": err.Error()})
 		return
 	}
+
+	if err := db.Create(&category).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create category", "details": err.Error()})
+		return
+	}
+
 	c.JSON(http.StatusCreated, category)
 }
+
 func UpdateCategory(c *gin.Context, db *gorm.DB) {
 	var updatedCategory models.Category
 	if err := c.ShouldBindJSON(&updatedCategory); err != nil {
@@ -66,6 +78,7 @@ func UpdateCategory(c *gin.Context, db *gorm.DB) {
 	}
 	c.JSON(http.StatusOK, updatedCategory)
 }
+
 func DeleteCategory(c *gin.Context, db *gorm.DB) {
 	id := c.Param("id")
 	if err := db.Where("id = ?", id).First(&models.Category{}).Error; err != nil {
@@ -77,4 +90,14 @@ func DeleteCategory(c *gin.Context, db *gorm.DB) {
 		return
 	}
 	c.JSON(http.StatusNoContent, nil)
+}
+
+func checkCategory(category models.Category, newCategory models.Category, db *gorm.DB) (bool, error) {
+	switch true {
+	case !category.SetName(newCategory.Name):
+		return true, fmt.Errorf("name is wrong formatted")
+	case !category.SetDescription(newCategory.Description):
+		return true, fmt.Errorf("description is wrong formatted")
+	}
+	return false, nil
 }
